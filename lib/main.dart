21e9105111bca50446bfc87cb36ebbd82a7b9083
Carl Lee <ckdwkdl91@gmail.com>
2025-08-22 16:51:47 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  debugPrint('백그라운드 메시지 수신: $message');
+}
+
+Future<void> setupFcm() async {
+  final fcm = FirebaseMessaging.instance;
+
+  await fcm.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  try {
+    final String? fcmToken = await fcm.getToken();
+
+    if (fcmToken != null) {
+      debugPrint("✅ FCM Token입니다!!! 이걸 넣으면 됩니다: $fcmToken");
+    } else {
+      debugPrint("🚨 토큰 없음");
+    }
+
+    fcm.onTokenRefresh.listen((newToken) {
+      debugPrint("🔄 New FCM Token: $newToken");
+    });
+  } catch (e) {
+    debugPrint("🔥 FCM Token Error: $e");
+  }
+}
+
+void main() async {
+  await Firebase.initializeApp(
+    //TODO: 파이어베이스 옵션 제작 후
+    //options: DefaultFirebaseOptions.currentPlatform
+  );
+
+  await setupFcm();
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {});
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('푸시테스트')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
